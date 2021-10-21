ceb243b0fac301fe6259151f9eda48b0e3c8b30b
eduard.poteshnov <eduard.poteshnov@wildix.com>
2021-10-21 19:01:57 +0300

    hw10

--- hwLesson3/js/hw10ConditionsV3.js
+++ hwLesson3/js/hw10ConditionsV3.js
@@ -22,17 +22,52 @@ if ((num % 2) === 0) {
 // Например, "Число " + num + " однозначное положительное".
 // Достаточно будет определить, является ли число однозначным, двухзначным или трехзначным и более.
 
-alert(buildAnswerString(num));
+alert(`Число ${num} ${getDigitsCount(num)} ${positivityCheck(num)}`);
 
-function buildAnswerString(num){
-    return `Число ${num} ${getDigitsCount(num)} ${positivityCheck(num)}`;
+// 4. Определить какое из трех, введенных пользователем, чисел максимальное и вывести его на экран.
+// Пользователь вводит поочерёдно через prompt числа.
+// Используя конструкцию if..else, напишите код, который должен определять,
+// какое из введенных чисел является наибольшим.
+
+alert("Еще один фокус впереди, введите 3 числа и получи в ответ самое большое")
+
+const num1 = getNumberFromUser();
+const num2 = getNumberFromUser();
+const num3 = getNumberFromUser();
+
+alert(`Возвращаю наибольшее ${getBigger(getBigger(num1, num2), num3)}. Остальные оставлю у себя`);
+
+//5. У треугольника сумма любых двух сторон должна быть больше третьей.
+// Иначе две стороны просто <лягут> на третью и треугольника не получится.
+// Пользователь вводит поочерёдно через prompt длины трех сторон.
+// Используя конструкцию if..else, напишите код, который должен определять,
+// может ли существовать треугольник при таких длинах.
+// Т. е. нужно сравнить суммы двух любых строн с оставшейся третьей стороной.
+// Чтобы треугольник существовал, сумма всегда должна быть больше отдельной стороны.
+
+alert("Еще один раз попрошу, ввести 3 числа и я тебе скажу получится " +
+    "построить треугольник с такими сторонами или нет, нажми OK как будешь готов.")
+
+const triSide1 = getNumberFromUser();
+const triSide2 = getNumberFromUser();
+const triSide3 = getNumberFromUser();
+const successMessage = `Треугольник со сторонами ${triSide1}, ${triSide2} и ${triSide1} получится`;
+const unsuccessMessage = `Треугольник со сторонами ${triSide1}, ${triSide2} и ${triSide1} неполучится`;
+
+if ((triSide1 + triSide2) > triSide3) {
+    alert(successMessage);
+} else {
+    alert(unsuccessMessage);
 }
 
-function removeMinus(num) {
-    if (num < 0) {
-        return num.toString().length - 1;
+alert((triSide1 + triSide2) > triSide3 ? successMessage : unsuccessMessage);
+
+
+function getBigger(num1, num2) {
+    if (num1 >= num2) {
+        return num1
     } else {
-        return num.toString().length;
+        return num2
     }
 }
 
@@ -57,6 +92,14 @@ function positivityCheck(num) {
             return "отрицательное";
         default:
             return "ноль"
+    }
+}
+
+function removeMinus(num) {
+    if (num < 0) {
+        return num.toString().length - 1;
+    } else {
+        return num.toString().length;
     }
 }
 
