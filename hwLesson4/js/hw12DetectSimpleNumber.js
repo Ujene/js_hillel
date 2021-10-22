344b11d1bf5582733436e04c94c55931c079c0e5
eduard.poteshnov <eduard.poteshnov@wildix.com>
2021-10-22 13:58:00 +0300

    hw12

---
+++ hwLesson4/js/hw12DetectSimpleNumber.js
@@ -0,0 +1,24 @@
+// Запросить у пользователя число, если число является простым то выводим в консоль true иначе false
+
+while (true) {
+    const num = prompt("Введите число и посмотрите в консоли простое ли оно. true - простое, false - сложное");
+    if (!isNaN(Number(num)) && num !== null && num.length > 0) {
+        console.log(isNumberSimple(num));
+    } else {
+        alert("В поле ввода попала какая-то ошибка, попробуйте обновить страницу и сможете снова играться");
+        break;
+    }
+}
+
+function isNumberSimple(num) {
+    if (num === 0) {
+        return "вы ввели ноль, а это не тру. Я незнаю простое это число или нет"
+    } else {
+        for (let i = 2; i < num; i++) {
+            if ((num % i) === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
