04623e967f978ec3a98757becc043547e19cdd85
eduard.poteshnov <eduard.poteshnov@wildix.com>
2021-12-15 17:35:38 +0200

    hw29 Looks like done

---
+++ hwLesson16/hw29ThisV2/scripts/script.js
@@ -0,0 +1,23 @@
+// Создайте объект calculator (калькулятор) с тремя методами:
+//
+// read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+// sum() (суммировать) возвращает сумму сохранённых значений.
+// mul() (умножить) перемножает сохранённые значения и возвращает результат.
+
+const calculator = {
+    operandA : 0,
+    operandB : 0,
+    read : (a, b) => {
+        if (typeof a !== 'number' || typeof b !== 'number' || Number.isNaN(a) || Number.isNaN(b)){
+            throw new Error("Arguments should have type number")
+        }
+        this.operandA = a;
+        this.operandB = b;
+    },
+    sum : () => {
+        return this.operandA + this.operandB;
+    },
+    mul : () => {
+        return this.operandA * this.operandB;
+    }
+}
