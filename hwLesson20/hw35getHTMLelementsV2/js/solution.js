cd91cb9317ea86fec1087a505c199edb8e50b733
eduard.poteshnov <eduard.poteshnov@wildix.com>
2021-12-22 19:00:35 +0200

    hw35 Saving the progress before going to home

--- hwLesson20/hw35getHTMLelementsV2/js/solution.js
+++ hwLesson20/hw35getHTMLelementsV2/js/solution.js
@@ -13,7 +13,21 @@ function insertToHtml(arr){
     return result;
 }
 
+function buildTable(rows, columns){
+    let result = '';
+    let semiResult = '';
 
+    for (let i = 0; i < rows; i++){
+        semiResult = '';
+        for (let j = 0; j < columns; j++){
+            semiResult = semiResult.concat(`<td style="border: solid 1px lightcyan;">${(i+1) * (j+1)}</td>`);
+        }
+        result = result.concat(`<tr>${semiResult}</tr>`)
+    }
+    return result;
+}
 
+document.body.insertAdjacentHTML("afterend", `<table>${buildTable(10,10)}</table>`)
+document.body.insertAdjacentHTML("afterend", `<p style="margin-left: 50px; font-size: 24px">Табличка 10 на 10</p>`)
 document.body.insertAdjacentHTML("afterend", `<ul>${insertToHtml(arr)}</ul>`);
-document.body.insertAdjacentHTML("afterend", `<p>Result of building list from array ${arr}</p>`);
+document.body.insertAdjacentHTML("afterend", `<p style="margin-left: 50px; font-size: 24px">Result of building list from array ${arr}</p>`);
