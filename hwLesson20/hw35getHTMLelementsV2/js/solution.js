bfb439dd10e31cbab330999b4de9f94aed603e13
eduard.poteshnov <eduard.poteshnov@wildix.com>
2021-12-23 14:57:59 +0200

    hw35 Semi result

--- hwLesson20/hw35getHTMLelementsV2/js/solution.js
+++ hwLesson20/hw35getHTMLelementsV2/js/solution.js
@@ -11,8 +11,6 @@ const buttonToConsole = document.getElementById("infoToConsole")
 
 const arr = [1, 2, 3, [4, 5, [6, 7], 8, 9], 10, 11];
 
-
-
 buttonArray.onclick = () => {
     customDiv.innerHTML = `<ul>${insertToHtml(arr)}</ul>`;
 }
